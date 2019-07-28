25dd1d9378fd4ef154162bea8d8c4f17ec09396d
Bright <kapyabright@outlook.com>
2019-07-28 16:55:30 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; //style sheet
 
-function App() {
+const App = () => {
+  const [html, setHtml] = useState('');
+  const handleChange = e => {
+    setHtml({ html: e });
+  };
+
+  const handleDone = () => {
+    console.log(html);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactQuill
+        onChange={e => handleChange(e)}
+        /* value={html} */
+        modules={App.modules}
+        formats={App.formats}
+        bounds={'.app'}
+        placeholder={'Tell your story'}
+      />
+      <button
+        onClick={() => {
+          handleDone();
+        }}
+        className="done-editting-btn"
+      >
+        Save
+      </button>
     </div>
   );
-}
+};
+
+App.modules = {
+  toolbar: [
+    [{ header: '1' }, { header: '2' }],
+    [{ size: [] }],
+    ['bold', 'italic', 'underline', 'blockquote'],
+    [
+      { list: 'ordered' },
+      { list: 'bullet' },
+      { indent: '-1' },
+      { indent: '+1' }
+    ],
+    ['link'],
+    ['clean']
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false
+  }
+};
+
+App.formats = [
+  'header',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link'
+];
 
 export default App;
